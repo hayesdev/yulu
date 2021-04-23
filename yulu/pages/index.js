e17f7df0f18fbdb90c17c3d82ba52452b2fd456f
Greg Hayes <gthelight@gmail.com>
2021-04-23 15:45:39 -0400

    created header with icons

--- yulu/pages/index.js
+++ yulu/pages/index.js
@@ -1,13 +1,17 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Yulu</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Building hulu clone with Next.js</h1>
+
+      {/* header */}
+      <Header />
+      {/* nav */}
     </div>
   );
 }
